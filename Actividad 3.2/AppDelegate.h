f43f2aa085afe3287972572a48313f46fe2d807d
javi <javi>
2014-10-26 11:31:20 +0100

    Initial Commit

---
+++ Actividad 3.2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Actividad 3.2
+//
+//  Created by Javi on 26/10/14.
+//  Copyright (c) 2014 Javi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Actividad 3.2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Actividad 3.2
+//
+//  Created by Javi on 26/10/14.
+//  Copyright (c) 2014 Javi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
